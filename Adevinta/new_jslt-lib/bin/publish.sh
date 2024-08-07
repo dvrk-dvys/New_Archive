d1db68d94771f3c147b0ebbc3d08483dc4378aa6
dvrk-dvys <ja.harr91@gmail.com>
2024-08-07 15:09:01 +0200

    new jslt

--- Adevinta/new_jslt-lib/bin/publish.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-./gradlew -PscalaVersion="2.11.11" publish tricklerdowner
-./gradlew -PscalaVersion="2.12.8" publish tricklerdowner
--- Adevinta/new_jslt-lib/bin/test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-./gradlew -PscalaVersion="2.11.11" check --parallel
-./gradlew -PscalaVersion="2.12.8" check --parallel
